658d70fc86b60334dc47c70c497acb9349b2c9ee
orijinnCHD <reussir.startup@gmail.com>
2022-10-31 16:05:00 +0400

    install environement test(test-helper, hardhat-truffle5, coverage' gas-reporter)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+require('solidity-coverage');
+require("@nomiclabs/hardhat-truffle5");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  settings: {          // See the solidity docs for advice about optimization and evmVersion
+    optimizer: {
+      enabled: false,
+      runs: 200
+    },
+    
+   },
+  gasReporter:{
+    enabled:true,
+    currency:'USD'
+ },
+
+
 };
