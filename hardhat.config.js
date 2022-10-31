712241a347177bfec48afdfbcf91ea6e73454339
orijinnCHD <reussir.startup@gmail.com>
2022-10-31 23:55:59 +0400

    deploy completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,7 @@ module.exports = {
   networks:{
     goerli:{
       url:`https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
-      accounts: [`0x${process.env._PRIVATE_KEY}`],
+      accounts: [`${process.env.PRIVATE_KEY}`],
       
     }
   },
